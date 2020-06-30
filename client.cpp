fa2b244ee321915c1da9b0707d9e6fb1bc409e90
Matt Aguilar <matta628@gmail.com>
2020-06-30 15:38:11 -0400

    Establish TCP communication successfully

--- client.cpp
+++ client.cpp
@@ -1,4 +1,4 @@
-// UDP Pinger
+// TCP server client simulation
 
 #include <iostream>
 #include <stdlib.h> 
@@ -11,63 +11,70 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-#define PORT	 12000
-
 int main(int argc, char ** argv) { 
 	int sockfd;
 	socklen_t len;
-	char buffer[1024];
+	char message[1024];
+	char acknowledge[1024];
 	struct sockaddr_in servaddr; 
 
 	// Check for correct format
-	if (argc != 2 ) return -1;
+	if (argc != 5 ){
+		printf("must be in format: ./client client_ID client_name server_name/ip portnumber\n");
+		return -1;
+	}
+
+	// Use command line args
+	char * clientID = argv[1];
+	char * clientName = argv[2];
+	char * serverName = argv[3];
+	int port = atoi(argv[4]);
 	
-	// Create a UDP socket
-	// Notice the use of SOCK_DGRAM for UDP packets
-	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-	
+	// Build message to send
+	std::string msg ("Client ");
+	msg += clientID;
+	msg.append(": ");
+	msg += clientName;
+	strcpy(message, msg.c_str());
+
+	// Create a TCP socket
+	// Notice the use of SOCK_STREAM for TCP packets
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	// Initialize servaddr to 0
 	memset(&servaddr, 0, sizeof(servaddr)); 
 
 	// Fill server information 
 	servaddr.sin_family = AF_INET; // IPv4
-	struct hostent * host = gethostbyname(argv[1]);
+	struct hostent * host = gethostbyname(serverName);
 	servaddr.sin_addr = *(struct in_addr*)(host->h_addr);
-	servaddr.sin_port = htons(PORT); // port number
+	servaddr.sin_port = htons(port); // port number
 	
 	// Connect to the server socket
-	connect(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr));
-
-	// Set socket timeout time
-	struct timeval  timeout;
-	timeout.tv_sec = 1;
-	timeout.tv_usec = 0;
-	setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(struct timeval));
-
-	// Write Message into Buffer
-	strcpy(buffer, "ping");
-
-	for (int i = 0; i < 10; i++){
-		//Get beginning time
-		clock_t ping = clock();
-
-		//Send packet to server
-		sendto(sockfd, (const char *)buffer, strlen(buffer), 
-			   MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
-		
-		//Receive the packet from server
-		int rec = recvfrom(sockfd, (char *)buffer, sizeof(buffer), 
-			MSG_WAITALL, ( struct sockaddr *) &servaddr, &len);
-		
-		if (rec < 0){
-			//Print Error Message
-			printf("Packet Lost\n");
-		}
-		else{
-			//Get end time
-			clock_t pong = clock();
-			//Print RTT Time
-			printf("RTT: %.3f ms\n", (double)(pong - ping) * 1000 / CLOCKS_PER_SEC);
-		}
+	if (connect(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0){
+		printf("connect error\n");
+		return -1;
 	}
+
+	// Send message to server
+	if (send(sockfd, (const char *)message, strlen(message), 0) < 0){
+		printf("send error\n");
+		return -1;
+	}
+		
+	// Receive acknowledgement from server
+	int rec = recv(sockfd, (char *)acknowledge, sizeof(acknowledge), 0);
+
+	// Print Message & Response
+	if (rec < 0){
+		printf("recv error\n");
+		return -1;
+	}
+	else{
+		acknowledge[rec] = '\0';
+		std::cout << "Message sent ==> " << message << std::endl;
+		std::cout << "Reply received ==> " << acknowledge << std::endl;
+	}
+	
 	return 0; 
 }
--- server.cpp
+++ server.cpp
@@ -1,6 +1,6 @@
-// UDP Pinger
+// TCP server client simulation
 
-// Must have this server running before you can run the UDP Pinger Client code
+// Must have this server running before you can run the TCP Client code
 
 #include <iostream>
 #include <stdlib.h> 
@@ -12,45 +12,114 @@
 #include <arpa/inet.h> 
 #include <netinet/in.h> 
 
-#define PORT	 12000
 
-int main() { 
+int main(int argc, char ** argv) { 
+	int port;
 	int sockfd, n;
 	socklen_t len;
-	char buffer[1024];
-	struct sockaddr_in servaddr, cliaddr; 
+	char acknowledge[1024];
+	char message1[1024];
+	char message2[1024];
+	struct sockaddr_in servaddr, cliaddr1, cliaddr2; 
+
+	// Check correct command line format
+	if (argc != 2){
+		printf("must be in format: ./server port_number\n");
+		return -1;
+	}
 	
-	// Create a UDP socket
-	// Notice the use of SOCK_DGRAM for UDP packets
-	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	// Set port number
+	port = atoi(argv[1]);
+	std::cout << port << std::endl;
 	
+	// Create a TCP socket
+	// Notice the use of SOCK_STREAM for TCP packets
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	// Initialize servaddr, cliaddr1, and cliaddr2 to 0
 	memset(&servaddr, 0, sizeof(servaddr)); 
-	memset(&cliaddr, 0, sizeof(cliaddr)); 
+	memset(&cliaddr1, 0, sizeof(cliaddr1));
+	memset(&cliaddr2, 0, sizeof(cliaddr2));
 	
 	// Fill server information 
 	servaddr.sin_family = AF_INET; // IPv4 
 	servaddr.sin_addr.s_addr = INADDR_ANY; // localhost
-	servaddr.sin_port = htons(PORT); // port number
+	servaddr.sin_port = htons(port); // port number
 	
 	// Bind the socket with the server address 
-	bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr));
-	
-	// random generator
-	srand(time(0));
-
-    while (1) {
-		//Receive the client packet along with the address it is coming from
-		n = recvfrom(sockfd, (char *)buffer, sizeof(buffer), 
-			MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len);
-		buffer[n] = '\0';
-
-		//If a random number in the range of 0 to 10 is less than 4,
-		//we consider the packet lost and do not respond
-		if (rand()%10 < 4) continue;
-
-		//Otherwise, the server responds
-		sendto(sockfd, (const char *)buffer, strlen(buffer), 
-			   MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len);
+	if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0){
+		printf("bind error\n");
+		return -1;
 	}
+
+	// Listen for any potential connections
+	if (listen(sockfd, 2) < 0){
+		printf("listen error\n");
+		return -1;
+	}
+
+	// Accept first connection
+	int newsockfd1 = accept(sockfd, (struct sockaddr *) &cliaddr1, &len);
+	if (newsockfd1 < 0){
+		printf("first accept error\n");
+		return -1;
+	}
+	
+	// Receive the first client message along with the address it is coming from
+	n = recv(newsockfd1, (char *)message1, sizeof(message1), 0);
+	if (n < 0){
+		printf("first recv error\t\t");
+		return -1;
+	}
+	message1[n] = '\0';
+
+	// Print first message
+	std::cout << message1 << std::endl;
+
+	// Accept second connection
+	int newsockfd2 = accept(sockfd, (struct sockaddr *) &cliaddr2, &len);
+	if (newsockfd2 < 0){
+		printf("second accept error\n");
+		return -1;
+	}
+	
+	// Receive the second client message along with the address it is coming from
+	n = recv(newsockfd2, (char *)message2, sizeof(message2), 0);
+	if (n < 0){
+		printf("second recv error\n");
+		return -1;
+	}
+	message2[n] ='\0';
+
+	// Print second message
+	std::cout << message2 << std::endl;
+
+	// Build acknowledgement message & parse client IDs
+	std::string msg1(message1);
+	std::string id1 = msg1.substr(7, msg1.find(':')-7);
+	std::string msg2(message2);
+	std::string id2 = msg2.substr(7, msg2.find(':')-7);
+	msg1 = msg1.substr(7) + " received before " + msg2.substr(7);
+	strcpy(acknowledge, msg1.c_str());
+	
+	// Send acknowledgment to first client
+	if (send(newsockfd1, (const char *)acknowledge, strlen(acknowledge), 0) < 0){
+		printf("first send error\n");
+		return -1;
+	}
+
+	// Send acknowledgment to second client
+	if (send(newsockfd2, (const char *)acknowledge, strlen(acknowledge), 0) < 0){
+		printf("second send error\n");
+		return -1;
+	}
+	std::cout << "Sent acknowledgements to both " << id1 << " and " << id2  << std::endl;
+
+	// Shutdown socket
+	if (shutdown(sockfd, SHUT_RDWR) < 0){
+		printf("shutdown error\n");
+		return -1;
+	}
+	
 	return 0; 
 }
